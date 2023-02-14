0464e82b9c4733646a35508b95ffc29eeace5938
Pranav Prashant <97225953+ipranavprashant@users.noreply.github.com>
2023-02-14 11:07:56 +0530

    Update ASSG2_B210460CS_CS03_PRANAV_5.c

--- Assignment 2/Answers/ASSG2_B210460CS_CS03_PRANAV_5.c
+++ Assignment 2/Answers/ASSG2_B210460CS_CS03_PRANAV_5.c
@@ -51,57 +51,6 @@ int BalanceFactor(struct Node *p) {
     // return hl - hr;
 }
 
-struct Node *createNode(int key)
-{
-    struct Node *temp=(struct Node *)malloc(sizeof(struct Node));
-    temp->data=key;
-    temp->height=1;
-    temp->lchild=NULL;
-    temp->rchild=NULL;
-    return temp;
-}
-
-struct Node *Insert(int key)
-{
-    if(root==NULL)
-    return createNode(key);
-    else if(key<root->data)
-    root->lchild=Insert(key);
-    else
-    root->rchild=Insert(key);
-}
-
-// void Insert(int key)
-// {
-//     struct Node *t = root;
-//     struct Node *r = NULL, *p;
-//     if (root == NULL)
-//     {
-//         p = (struct Node *)malloc(sizeof(struct Node));
-//         p->data = key;
-//         p->height = 1;
-//         p->lchild = p->rchild = NULL;
-//         root = p;
-//         return;
-//     }
-//     while (t != NULL)
-//     {
-//         r = t;
-//         if (key < t->data)
-//             t = t->lchild;
-//         else if (key > t->data)
-//             t = t->rchild;
-//         else
-//             return;
-//     }
-//     p = (struct Node *)malloc(sizeof(struct Node));
-//     p->data = key;
-//     p->lchild = p->rchild = NULL;
-//     if (key < r->data)
-//         r->lchild = p;
-//     else
-//         r->rchild = p;
-// }
 struct Node *LLRotation(struct Node *p) {
     struct Node* pl = p->lchild;
     struct Node* plr = pl->rchild;
@@ -169,16 +118,22 @@ struct Node *RLRotation(struct Node *p) {
     return prl;
 }
 
+
+struct Node *createNode(int key)
+{
+    struct Node *temp=(struct Node *)malloc(sizeof(struct Node));
+    temp->data=key;
+    temp->height=1;
+    temp->lchild=NULL;
+    temp->rchild=NULL;
+    return temp;
+}
+
 struct Node *RInsert(struct Node *p, int key)
 {
     struct Node *t;
    if (p == NULL){
-        t = (struct Node *)malloc(sizeof(struct Node));
-        t->data = key;
-        t->lchild = NULL;
-        t->rchild = NULL;
-        t->height = 1;
-        return t;
+return createNode(key);
     }
  
     if (key < p->data){
@@ -201,6 +156,8 @@ struct Node *RInsert(struct Node *p, int key)
  
     return p;
 }
+
+
 void Inorder(struct Node *p)
 {
     if (p)
