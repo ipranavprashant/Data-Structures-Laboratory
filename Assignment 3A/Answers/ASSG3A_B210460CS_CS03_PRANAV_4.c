8290f6b1af8ef3158f9abe91a82c8648a7662745
Pranav Prashant <97225953+ipranavprashant@users.noreply.github.com>
2023-03-27 23:04:14 +0530

    Update ASSG3A_B210460CS_CS03_PRANAV_4.c

--- Assignment 3A/Answers/ASSG3A_B210460CS_CS03_PRANAV_4.c
+++ Assignment 3A/Answers/ASSG3A_B210460CS_CS03_PRANAV_4.c
@@ -15,8 +15,9 @@ void insertionSort(int arr[], int size) {
 
 int min_cost_to_connect_ropes(int ropes[], int n) {
   int total_cost = 0;
-  insertionSort(ropes, n);
+  //insertionSort(ropes, n);
   while (n > 1) {
+    insertionSort(ropes, n);
     int cost = ropes[0] + ropes[1];
     total_cost += cost;
     ropes[0] = cost;
